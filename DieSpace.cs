60ea3b8951a532a268fc8d9ed203548f8b1292ec
kozachka <kozachka@ukr.net>
2023-01-08 17:48:22 +0200

    Add files via upload

---
+++ DieSpace.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DieSpace : MonoBehaviour
+{
+    public GameObject respawn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = respawn.transform.position;
+        }
+    }
+}
---
+++ PlayerCtrl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody2D rb;
+    private bool faceRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * 7000);
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+
+    }
+    void flip()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+
+}
